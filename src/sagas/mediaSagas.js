838d94b832b2af444328edfa2f201882beb0e3b9
Yoo La Sung <yoola93@gmail.com>
2017-02-17 13:02:31 -0800

    initial commit

---
+++ src/sagas/mediaSagas.js
@@ -0,0 +1,16 @@
+import { put, call } from 'redux-saga/effects';
+import { flickrImages } from '../Api/api';
+import * as types from '../constants/actionTypes';
+
+
+export default function* searchMediaSaga({ payload }) {
+  try {
+        const images = yield call(flickrImages, payload);
+    yield [
+      put({ type: types.FLICKR_IMAGES_SUCCESS, images }),
+      put({ type: types.SELECTED_IMAGE, image: images[0] })
+    ];
+  } catch (error) {
+    yield put({ type: 'SEARCH_MEDIA_FAILURE', error });
+  }
+}
